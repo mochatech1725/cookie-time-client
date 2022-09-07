23092b26119e47d722276fc3bb53b5c997c94d56
TeialD <teial.starks@gmail.com>
2022-09-06 22:34:34 -0700

    Stuff

--- src/services/agent.service.js
+++ src/services/agent.service.js
@@ -1,36 +1,44 @@
-import axios from 'axios';
-import {SERVER_BASE_URL} from '../utils/constants.js'
+import axios from "axios";
+import { SERVER_BASE_URL } from "../utils/constants.js";
 
-export class AgentService
- {
+export class AgentService {
+  static async getAgents() {
+    return axios
+      .get(`${SERVER_BASE_URL}/agent/get_agents`)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+        return [];
+      });
+  }
 
-    static async getAgents() {
-        return axios.get(`${SERVER_BASE_URL}/agent/get_agents`)
-         .then(response => {
-            console.log(response);
-         }).catch(err => {
-            console.log(err);
-            return [];
-         } );
-    }
+  static async getAgent(first_name, last_name) {
+    return axios
+      .get(
+        `${SERVER_BASE_URL}/agent/get_agent?first_name=${first_name}&last_name=${last_name}`
+      )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+        return [];
+      });
+  }
 
-    static async getAgent(first_name, lat_name) {
-        return axios.get(`${SERVER_BASE_URL}/agent/get_agent?first_name=${first_name}&last_name=${last_name}`)
-         .then(response => {
-            console.log(response);
-         }).catch(err => {
-            console.log(err);
-            return [];
-         });
-    }
-
-    static async addAgent(first_name, lat_name) {
-        return axios.get(`${SERVER_BASE_URL}/agent/addfirst_name=${first_name}&last_name=${last_name}`)
-         .then(response => {
-            console.log(response);
-         }).catch(err => {
-            console.log(err);
-            return [];
-         });
-    }
+  static async addAgent(first_name, lat_name) {
+    return axios
+      .get(
+        `${SERVER_BASE_URL}/agent/addfirst_name=${first_name}&last_name=${last_name}`
+      )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+        return [];
+      });
+  }
 }
--- src/services/order.service.js
+++ src/services/order.service.js
@@ -1,37 +1,58 @@
-import axios from 'axios';
-import {SERVER_BASE_URL} from '../utils/constants.js'
-
+import axios from "axios";
+import { SERVER_BASE_URL } from "../utils/constants.js";
 
 export class OrderService {
+  static async createCustomerOrderItem(
+    customer_id,
+    campaign_id,
+    product_id,
+    quantity
+  ) {
+    return axios
+      .get(
+        `${SERVER_BASE_URL}/order/createCustomerOrder?campaign_id=${campaign_id}&customer_id=${customer_id}&product_id=${product_id}&quantity=${quantity}`
+      )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+        return [];
+      });
+  }
 
-   static async createCustomerOrderItem(customer_id, campaign_id, product_id, quantity) {
-      return axios.get(`${SERVER_BASE_URL}/order/createCustomerOrder?campaign_id=${campaign_id}&customer_id=${customer_id}&product_id=${product_id}&quantity=${quantity}`)
-       .then(response => {
-          console.log(response);
-       }).catch(err => {
-          console.log(err);
-          return [];
-       } );
-   }
+  static async createCustomerOrder(
+    customer_id,
+    campaign_id,
+    order_source,
+    order_items
+  ) {
+    return axios
+      .get(
+        `${SERVER_BASE_URL}/order/createCustomerOrder?campaign_id=${campaign_id}&customer_id=${customer_id}&order_source=${order_source}&order_items=${JSON.stringify(
+          order_items
+        )}`
+      )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+        return [];
+      });
+  }
 
-   static async createCustomerOrder(customer_id, campaign_id, order_source, order_items) {
-      return axios.get(`${SERVER_BASE_URL}/order/createCustomerOrder?campaign_id=${campaign_id}&customer_id=${customer_id}&order_source=${order_source}&order_items=${JSON.stringify(order_items)}`)
-       .then(response => {
-          console.log(response);
-       }).catch(err => {
-          console.log(err);
-          return [];
-       } );
-   }
-
-
-    static async getCustomerOrders(campaign_id,customer_id) {
-        return axios.get(`${SERVER_BASE_URL}/order/get_customers_order?campaign_id=${campaign_id}&customer_id=${customer_id}`)
-         .then(response => {
-            console.log(response);
-         }).catch(err => {
-            console.log(err);
-            return [];
-         } );
-    }
+  static async getCustomerOrders(campaign_id, customer_id) {
+    return axios
+      .get(
+        `${SERVER_BASE_URL}/order/get_customers_order?campaign_id=${campaign_id}&customer_id=${customer_id}`
+      )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+        return [];
+      });
+  }
 }
