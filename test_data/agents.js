a4c284ddcb7e3dea92d71f1c7a63950c8afa1b1b
TeialD <teial.starks@gmail.com>
2022-09-08 08:14:50 -0700

    Add test data

---
+++ test_data/agents.js
@@ -0,0 +1,16 @@
+export const agent_data = {
+    "rows" : [
+    {
+        "id": "123",
+        "agent_id": "456",
+        "first_name": "Barrack",
+        "last_name": "Obama"
+    },
+    {
+        "id": "4888",
+        "agent_id": "9999",
+        "first_name": "Michelle",
+        "last_name": "Obama"
+    }
+    ]
+}
