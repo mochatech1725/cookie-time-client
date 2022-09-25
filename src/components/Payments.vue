541b405dae20a7cf2c580093580eaf40bac4f359
TeialD <teial.starks@gmail.com>
2022-09-24 17:19:02 -0700

    fix some stuff

--- src/components/Payments.vue
+++ src/components/Payments.vue
@@ -14,7 +14,7 @@
           <v-dialog v-model="dialog" max-width="500px" >
             <template v-slot:activator="{ on, attrs }">
               <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on" >
-                New Item
+                New Payment
               </v-btn>
             </template>
             <v-card>
