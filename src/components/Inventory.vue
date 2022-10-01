933cd4cac4be003c099884072ac6d18ef9adec61
TeialD <teial.starks@gmail.com>
2022-10-01 14:46:35 -0700

    revert changes to inventory screen

--- src/components/Inventory.vue
+++ src/components/Inventory.vue
@@ -4,8 +4,6 @@
       :headers="headers"
       :items="rows"
       class="elevation-1 mytable"
-      :hide-default-footer="true"
-
     >
       <template v-slot:top>
         <v-toolbar flat >
@@ -13,7 +11,11 @@
           <v-divider class="mx-4" inset vertical ></v-divider>
            <v-spacer></v-spacer>
           <v-dialog v-model="dialog" max-width="500px" >
-
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on" >
+                New Inventory
+              </v-btn>
+            </template>
             <v-card>
               <v-card-title>
                 <span class="text-h5">{{ formTitle }}</span>
@@ -70,11 +72,25 @@
               </v-card-actions>
             </v-card>
           </v-dialog>
+          <v-dialog v-model="dialogDelete" max-width="500px">
+            <v-card>
+              <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+                <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
         </v-toolbar>
       </template>
       <template v-slot:[`item.actions`]="{ item }">
         <v-icon small class="mr-2" @click="editItem(item)" >
           mdi-pencil
+        </v-icon>
+        <v-icon small @click="deleteItem(item)" >
+          mdi-delete
         </v-icon>
       </template>
     </v-data-table>
