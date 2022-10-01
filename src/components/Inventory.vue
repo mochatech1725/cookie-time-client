f7391eaf2d4ed365244373aa68af94429024fb86
TeialD <teial.starks@gmail.com>
2022-09-30 21:50:56 -0700

    Fix width of products

--- src/components/Inventory.vue
+++ src/components/Inventory.vue
@@ -3,8 +3,9 @@
     <v-data-table
       :headers="headers"
       :items="rows"
-      sort-by="order_date"
       class="elevation-1 mytable"
+      :hide-default-footer="true"
+
     >
       <template v-slot:top>
         <v-toolbar flat >
@@ -12,11 +13,7 @@
           <v-divider class="mx-4" inset vertical ></v-divider>
            <v-spacer></v-spacer>
           <v-dialog v-model="dialog" max-width="500px" >
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on" >
-                New Inventory
-              </v-btn>
-            </template>
+
             <v-card>
               <v-card-title>
                 <span class="text-h5">{{ formTitle }}</span>
@@ -73,25 +70,11 @@
               </v-card-actions>
             </v-card>
           </v-dialog>
-          <v-dialog v-model="dialogDelete" max-width="500px">
-            <v-card>
-              <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-                <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
-                <v-spacer></v-spacer>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
         </v-toolbar>
       </template>
       <template v-slot:[`item.actions`]="{ item }">
         <v-icon small class="mr-2" @click="editItem(item)" >
           mdi-pencil
-        </v-icon>
-        <v-icon small @click="deleteItem(item)" >
-          mdi-delete
         </v-icon>
       </template>
     </v-data-table>
--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,77 +1,82 @@
 <!-- eslint-disable vue/multi-word-component-names -->
-<template>
-    <v-data-table
-      :headers="headers"
-      :items="rows"
-      sort-by="product_name"
-      class="elevation-1 mytable"
-    >
-      <template v-slot:top >
-        <v-toolbar flat >
-          <v-toolbar-title>Products</v-toolbar-title>
-          <v-divider class="mx-4" inset vertical ></v-divider>
-           <v-spacer></v-spacer>
+<template >
+  <v-container fluid fill-height  class="d-flex align-center justify-center"  >
+    <v-card  fluid width="600px" color="green lighten-4" min-width="600px">
+      <v-data-table
+        :headers="headers"
+        :items="rows"
+        sort-by="product_name"
+        class="elevation-1 mytable"
+        :hide-default-footer="true"
+      >
+        <template v-slot:top >
+          <v-toolbar flat >
+            <v-toolbar-title>Products</v-toolbar-title>
+            <v-divider class="mx-4" inset vertical ></v-divider>
+            <v-spacer></v-spacer>
 
-          <v-dialog v-model="dialog" max-width="500px" >
+            <v-dialog v-model="dialog" max-width="500px" >
+              
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on" > New Product </v-btn>
+              </template>
+
+              <v-card>
+                <v-card-title>
+                  <span class="text-h5">{{ formTitle }}</span>
+                </v-card-title>
+
+                <v-card-text>
+                  <v-container>
+                    <v-row>
+                      <v-col cols="12" sm="6" md="4" >
+                        <v-text-field outlined v-model="editedItem.product_name" label="Product Name"></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6" md="4" >
+                        <v-text-field outlined v-model="editedItem.product_price" label="Price" ></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6" md="4" >
+                        <v-text-field outlined v-model="editedItem.year_introduced" label="Year" ></v-text-field>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
+    
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="close" > Cancel </v-btn>
+                  <v-btn color="blue darken-1" text @click="save" > Save </v-btn>
+                </v-card-actions>
+
+              </v-card>
+
+            </v-dialog>
+
+            <v-dialog v-model="dialogDelete" max-width="500px">
+              <v-card>
+                <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+                  <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                  <v-spacer></v-spacer>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
             
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on" > New Product </v-btn>
-            </template>
-
-            <v-card>
-              <v-card-title>
-                <span class="text-h5">{{ formTitle }}</span>
-              </v-card-title>
-
-              <v-card-text>
-                <v-container>
-                  <v-row>
-                    <v-col cols="12" sm="6" md="4" >
-                      <v-text-field outlined v-model="editedItem.product_name" label="Product Name"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4" >
-                      <v-text-field outlined v-model="editedItem.product_price" label="Price" ></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" md="4" >
-                      <v-text-field outlined v-model="editedItem.year_introduced" label="Year" ></v-text-field>
-                    </v-col>
-                  </v-row>
-                </v-container>
-              </v-card-text>
-  
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="close" > Cancel </v-btn>
-                <v-btn color="blue darken-1" text @click="save" > Save </v-btn>
-              </v-card-actions>
-
-            </v-card>
-
-          </v-dialog>
-
-          <v-dialog v-model="dialogDelete" max-width="500px">
-            <v-card>
-              <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-                <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
-                <v-spacer></v-spacer>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-          
-        </v-toolbar>
-      </template>
-      <template v-slot:[`item.product_image`]="{ item }">
-          <img :src="require('../assets/product-images/' + item.product_image)" style="width: 50px; height: 50px">
-      </template>
-               
-      <template v-slot:[`item.actions`]="{ item }">
-        <v-icon small class="mr-2" @click="editItem(item)" > mdi-pencil </v-icon>
-        <v-icon small @click="deleteItem(item)" > mdi-delete </v-icon>
-      </template>
-    </v-data-table>
+          </v-toolbar>
+        </template>
+        <template v-slot:[`item.product_image`]="{ item }">
+            <img :src="require('../assets/product-images/' + item.product_image)" style="width: 50px; height: 50px">
+        </template>
+                
+        <template v-slot:[`item.actions`]="{ item }">
+          <v-icon small class="mr-2" @click="editItem(item)" > mdi-pencil </v-icon>
+          <v-icon small @click="deleteItem(item)" > mdi-delete </v-icon>
+        </template>
+      </v-data-table>
+      </v-card>
+    </v-container>
   </template>
   
   <script>
